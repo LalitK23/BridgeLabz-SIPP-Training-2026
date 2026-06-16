b84ef7fd949a06aff19634997488464b6dc831f6
Lalit kumar <eshukl853@gmail.com>
2026-06-16 11:25:21 +0530

    Added Array Level2 programs

---
+++ core-java-practice/gcr-codebase/array/Level2/BMIArray.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BMIArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        double[] weight = new double[number];
+        double[] height = new double[number];
+        double[] bmi = new double[number];
+        String[] status = new String[number];
+
+        for (int i = 0; i < number; i++) {
+            weight[i] = sc.nextDouble();
+            height[i] = sc.nextDouble();
+
+            double meter = height[i] / 100;
+
+            bmi[i] = weight[i] / (meter * meter);
+
+            if (bmi[i] < 18.5)
+                status[i] = "Underweight";
+            else if (bmi[i] < 25)
+                status[i] = "Normal";
+            else if (bmi[i] < 30)
+                status[i] = "Overweight";
+            else
+                status[i] = "Obese";
+        }
+
+        for (int i = 0; i < number; i++) {
+            System.out.println(height[i] + " " + weight[i] + " " + bmi[i] + " " + status[i]);
+        }
+    }
+}
