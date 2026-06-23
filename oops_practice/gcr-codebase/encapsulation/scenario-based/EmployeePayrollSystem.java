0175bfa90ea7efe49f2b2c0e1169708ada375201
Lalit kumar <eshukl853@gmail.com>
2026-06-23 11:55:31 +0530

    soln of scenario based problem

---
+++ oops_practice/gcr-codebase/encapsulation/scenario-based/EmployeePayrollSystem.java
@@ -0,0 +1,121 @@
+abstract class Employee {
+
+    private int employeeId;
+    private String employeeName;
+
+    public Employee(int employeeId, String employeeName) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    abstract double calculateSalary();
+
+    public void displayEmployeeInfo() {
+
+        System.out.println("Employee ID : " + employeeId);
+
+        System.out.println("Employee Name : " + employeeName);
+    }
+}
+
+class FullTimeEmployee extends Employee {
+
+    private double monthlySalary;
+
+    public FullTimeEmployee(int employeeId, String employeeName, double monthlySalary) {
+
+        super(employeeId, employeeName);
+
+        this.monthlySalary = monthlySalary;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    double calculateSalary() {
+
+        return monthlySalary;
+    }
+}
+
+class PartTimeEmployee extends Employee {
+
+    private int hoursWorked;
+
+    private double hourlyRate;
+
+    public PartTimeEmployee(int employeeId, String employeeName, int hoursWorked, double hourlyRate) {
+
+        super(employeeId, employeeName);
+
+        this.hoursWorked = hoursWorked;
+
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    double calculateSalary() {
+
+        return hoursWorked * hourlyRate;
+    }
+}
+
+public class EmployeePayrollSystem {
+
+    public static void main(String[] args) {
+
+        FullTimeEmployee fullTimeEmployee =
+                new FullTimeEmployee(101, "Amit", 50000);
+
+        PartTimeEmployee partTimeEmployee =
+                new PartTimeEmployee(102, "Rahul", 80, 300);
+
+        fullTimeEmployee.displayEmployeeInfo();
+
+        System.out.println("Salary : " + fullTimeEmployee.calculateSalary());
+
+        System.out.println();
+
+        partTimeEmployee.displayEmployeeInfo();
+
+        System.out.println("Salary : " + partTimeEmployee.calculateSalary());
+    }
+}
